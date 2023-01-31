7f62295bdbc6dbf695bd129e9f9e194fdbf378f3
hayatali98 <83293020+hayatali98@users.noreply.github.com>
2023-01-31 16:28:58 +0530

    Update LaunchEC2.tf

--- LaunchEC2.tf
+++ LaunchEC2.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region  = "us-west-2"
-}
-
 resource "aws_instance" "web" {
   ami           = "ami-0253a8101a3b88872"
   instance_type = "t3.micro"
@@ -9,4 +5,8 @@ resource "aws_instance" "web" {
   tags = {
     Name = "HelloWorld"
   }
+}
+
+provider "aws" {
+  region  = "us-west-2"
 }
