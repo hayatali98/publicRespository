69c3e49dd28f7e1515546cfff675b627d76d8a14
hayatali98 <83293020+hayatali98@users.noreply.github.com>
2023-01-31 12:37:54 +0530

    Update LaunchEC2.tf

--- LaunchEC2.tf
+++ LaunchEC2.tf
@@ -1,23 +1,24 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
+data "aws_ami" "app_ami" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
   }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["979382823631"] # Bitnami
 }
 
-provider "aws" {
-  region                  = "us-east-1"
-  profile                 = "default"
-}
-
-resource "aws_vpc" "my_vpc" {
-  cidr_block           = "10.123.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.app_ami.id
+  instance_type = "t3.nano"
 
   tags = {
-    Name = "ec2_vpc"
+    Name = "HelloWorld"
   }
 }
