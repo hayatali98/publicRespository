22db087fb227ac32a9f1cf905e2ff4315220dab2
hayatali98 <83293020+hayatali98@users.noreply.github.com>
2023-01-31 16:27:56 +0530

    Update LaunchEC2.tf

--- LaunchEC2.tf
+++ LaunchEC2.tf
@@ -1,11 +1,12 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-    }
-  }
-}
-
 provider "aws" {
   region  = "us-west-2"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0253a8101a3b88872"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
